6bab0d4d953b6c19b6f9d8f39d7ae818a63865e1
Hai Tran <haits293@gmail.com>
2016-12-04 13:08:09 +0700

    create, update, destroy categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,58 @@
 class CategoriesController < ApplicationController
+  before_action :check_exist, except: [:index, :new, :create]
+
+  def index
+  end
+
+  def show
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new category_params
+    if @category.save
+      flash[:success] = "Category created successfully"
+      redirect_to categories_url
+    else
+      flash[:info] = "Something errors"
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @category.update_attributes category_params
+      flash[:success] = "Category updated"
+      redirect_to categories_url
+    else
+      flash[:info] = "Something errors"
+      render :edit
+    end
+  end
+
+  def destroy
+    @category.destroy
+    flash[:success] = "Category deleted"
+    redirect_to categories_url
+  end
+
+  private
+  def category_params
+    params.require(:category).permit :name
+  end
+
+  def check_exist
+    @category = Category.find_by id: params[:id]
+    if @category.nil?
+      flash[:info] = "This category does not exist"
+      redirect_to root_url
+    else
+      @category
+    end
+  end
 end
